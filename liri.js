76c4076f3008552b86b2b5ca5bec3774d8ededbb
Nilsen <58581719+nreyes1202@users.noreply.github.com>
2020-04-16 13:59:38 -0600

    added code for spotify, movie, bands

--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
+// Dependencies
+
 require("dotenv").config();
 var keys = require("./keys.js");
 
@@ -10,10 +12,59 @@ var moment = require("moment")
 
 // Grab user info
 var command = process.argv[2];
-console.log(process.argv[2])
+console.log(command)
 
-var query = process.argv[3];
-console.log(process.argv[3]);
-// check info
-// depending on info execute some code
-// calculator activity
+var query = process.argv.slice(3).join(" ");
+console.log(query);
+
+// Use Axios to get back results from bands in town (Concert-This)
+
+// if/else
+
+if (command == "concert-this") {
+
+    var queryURL = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp";
+    console.log(queryURL);
+
+    axios.get(queryURL).then(
+        function (bandResponse) {
+            console.log("Venue: " + bandResponse.data[0].venue.name);
+            console.log("City: " + bandResponse.data[0].venue.city);
+            console.log(moment(bandResponse.data[0].datetime).format("MM/DD/YYYY"));
+        });
+} else if (command == "spotify-this-song") { 
+    query = query || "The Sign by Ace of Base";
+    spotify.search({ type: 'track', query: query }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(data.tracks.items);
+    });
+} else if (command == "movie-this") {
+    query = query || "Mr. Nobody";
+
+    var queryURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + query;
+    console.log(queryURL);
+
+    axios.get(queryURL).then(
+        function (movieResponse) {
+            console.log(movieResponse.data);
+            // console.log("City: " + bandResponse.data[0].venue.city);
+            // console.log(moment(bandResponse.data[0].datetime).format("MM/DD/YYYY"));
+        });
+
+
+}
+
+
+// look into fs package
+// npm init to get package.json
+//require fs package
+
+
+
+;
+
+
+// Use switch-case statements?
+// define bandName
